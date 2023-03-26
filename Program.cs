fabc32c679f93223b7a7fc0e9097b8d8674306d1
ViniciusSouza1988 <viniciusdesouzasilveira1988@gmail.com>
2023-03-26 01:13:55 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.AccessControl;
+
+double Pi = 3.14159;
+
+double Raio = 0;
+Console.WriteLine("Qual tamanho do Raio, em CM ");
+Raio =  double.Parse(Console.ReadLine());
+
+double Altura = 0;
+Console.WriteLine("Qual a altura, em CM ");
+Altura = double.Parse(Console.ReadLine());
+
+double Volume = Pi * (Raio * Raio) * Altura;
+
+Console.WriteLine("-----------");
+
+Console.WriteLine("O Volume da Lata é de " + Volume + "cm cubicos");
